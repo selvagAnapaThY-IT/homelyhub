2ac5820a6e7c94879093be75d6dc1d48d8dc66b5
selvagApaThY-IT <selvaganapathyn23itpsnacet.edu.in>
2026-09-19 10:13:03 +0530

    udated backend url

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy:{
       '/api':{
-        target: 'http://localhost:8080',
+        target: 'https://homelyhub-g6bl.onrender.com',
         changeOrigin: true,
         secure: false,
       }
