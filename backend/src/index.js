0d9c49084a5b74f650483fa79b1bafc08ce07d74
selvagApaThY-IT <selvaganapathyn23itpsnacet.edu.in>
2026-09-19 10:55:47 +0530

    updated url

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({
     extended: true
 }));
 app.use(cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:3000"],
+    origin: ["https://homelyhubselva.netlify.app", "http://127.0.0.1:5173", "http://localhost:3000"],
     credentials: true,
 }));
 app.use(cookieParser());
